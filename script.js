b9a88bc21ed82e8f8b41c7d856b87b7a05a24e42
Afnan A. Abed <haeseongp@gmail.com>
2018-12-26 23:14:17 +0200

    Coded the form validation in js

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 function validateForm() {
-  var name = document.forms["contactForm"]["name"].value;
-  if (name == "") {
-    alert("Name must be filled out");
-  }
-}
+	var name = document.forms["contactForm"]["name"];               
+    var email = document.forms["contactForm"]["email"];    
+    var subject = document.forms["contactForm"]["subject"];  
+    var message =  document.forms["contactForm"]["message"];  
 
-function test() {
-	alert("item clicked")
+    if (name.value == "") { 
+        window.alert("Name must be filled out"); 
+        name.focus(); 
+        return false; 
+	} 
+
+    if (email.value == "") {                           
+        window.alert("Email must be filled out"); 
+        email.focus(); 
+        return false; 
+    } 
+
+    if (subject.value == "") { 
+        window.alert("You must specify a subject"); 
+        subject.focus(); 
+        return false; 
+	} 
+
+    if (message.value == "") {                           
+        window.alert("You must write a message"); 
+        message.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf("@", 0) < 0) {
+        window.alert("You must enter a valid e-mail address"); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0) {
+        window.alert("You must enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    return true; 
+    
 }
